9c9a0da879bfee4d8dcff0af8875f70dac361c38
MaJoseSS <136508885+MaJoseSS@users.noreply.github.com>
2025-06-16 01:18:54 -0700

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -13,26 +13,27 @@ module tb ();
     #1;
   end
 
-  // Wire up the inputs and outputs:
-  reg clk;
-  reg rst_n;
-  reg ena;
-  reg [7:0] ui_in;
-  reg [7:0] uio_in;
-  wire [7:0] uo_out;
-  wire [7:0] uio_out;
-  wire [7:0] uio_oe;
+ 
+    output wire [7:0] uo_out,
+    input  wire [7:0] ui_in,
+    output wire [7:0] uio_out,
+    output wire [7:0] uio_oe,
+    input  wire [7:0] uio_in,
+    input  wire       ena,
+    input  wire       clk,
+    input  wire       rst_n
+);
 
-  // Replace tt_um_example with your module name:
-  tt_um_example user_project (
-      .ui_in  (ui_in),    // Dedicated inputs
-      .uo_out (uo_out),   // Dedicated outputs
-      .uio_in (uio_in),   // IOs: Input path
-      .uio_out(uio_out),  // IOs: Output path
-      .uio_oe (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
-      .ena    (ena),      // enable - goes high when design is selected
-      .clk    (clk),      // clock
-      .rst_n  (rst_n)     // not reset
-  );
+// Cambiar "tt_um_example" por "tt_um_uart"
+tt_um_uart tt_um_uart (
+    .ui_in      (ui_in),
+    .uo_out     (uo_out),
+    .uio_in     (uio_in),
+    .uio_out    (uio_out),
+    .uio_oe     (uio_oe),
+    .ena        (ena),
+    .clk        (clk),
+    .rst_n      (rst_n)
+);
 
 endmodule
